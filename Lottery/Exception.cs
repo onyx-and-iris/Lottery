1b4e0baebb2a706a8748199efcb9339b029ce106
onyx-and-iris <code@onyxandiris.online>
2024-09-26 21:04:23 +0100

    add base exception class

---
+++ Lottery/Exception.cs
@@ -0,0 +1,9 @@
+﻿namespace Lottery
+{
+    /// <summary>
+    /// Base Exception class for the LottoPicker app
+    /// </summary>
+    internal class LottoPickerException(string message) : Exception(message)
+    {
+    }
+}
